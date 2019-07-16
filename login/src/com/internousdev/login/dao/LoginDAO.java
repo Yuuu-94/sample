df6273efdd2f30f8cbb48d3c01be487003df0975
Yuuu-94 <yuuujam84@gmail.com>
2019-07-16 10:43:55 +0900

    d

--- login/src/com/internousdev/login/dao/LoginDAO.java
+++ login/src/com/internousdev/login/dao/LoginDAO.java
@@ -1,5 +1,4 @@
-//検索エンジン(データアクセス）みたいな場所！！
-//DTO二格納した情報をActionクラスへ渡す
+
 
 package com.internousdev.login.dao;
 import java.sql.Connection;
@@ -12,19 +11,19 @@ import com.internousdev.login.util.DBConnector;
 
 public class LoginDAO {
 	public LoginDTO select(String name,String password) throws SQLException{
-		//→loginActionから
+	
 		LoginDTO dto=new LoginDTO();
 		DBConnector db=new DBConnector();
 		Connection con=db.getConnection();
 
 		String sql="select * from user where user_name=? and password=?";
-//値は？と記載
+
 	try{
-		PreparedStatement ps=con.prepareStatement(sql);//DB間で運んでくれる箱に代入
-		ps.setString(1, name);//user_nameの？にAction→のname
-		ps.setString(2,password);//passwordの？にAction→のpassword
+		PreparedStatement ps=con.prepareStatement(sql);
+		ps.setString(1, name);
+		ps.setString(2,password);
 		ResultSet rs=ps.executeQuery();
-		//select文のSQLを実行するメソッド.戻り値はresultset
+		
 		
 
 		if(rs.next()){
@@ -33,11 +32,11 @@ public class LoginDAO {
 		}
 	}catch(SQLException e){
 		e.printStackTrace();
-		//エラー処理
+		
 	}finally{
 		con.close();
-	}//データーベース接続終了の際に書くメソッド
-	return dto;//dtoにはいった値を→Actionクラスへ
+	}
+	return dto;
 	}
 
 }
